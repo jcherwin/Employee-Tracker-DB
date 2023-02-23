f18226243425cbf44b20c8ebbd78d3d43370e732
Duke Nukem <joncherwin@gmail.com>
2023-02-22 18:29:23 -0600

    Setup inquirer prompts and created files for API routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,6 +22,8 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the company_db database.`)
 );
+
+app.use('/api', api);
 
 // Create a movie
 app.post('/api/new-movie', ({ body }, res) => {
